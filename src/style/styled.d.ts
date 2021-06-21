21400ea1899c87bd922309c5b5c945bcefad6a4e
budzswap <85272529+budzswap@users.noreply.github.com>
2021-06-21 12:23:03 -0500

    Fixed compiler errors.

--- src/style/styled.d.ts
+++ src/style/styled.d.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/no-unresolved
-import { BudzTheme } from '@pantherswap-libs/uikit/dist/theme'
+import { PantherTheme } from '@pantherswap-libs/uikit/dist/theme'
 
 declare module 'styled-components' {
   /* eslint-disable @typescript-eslint/no-empty-interface */
-  export interface DefaultTheme extends BudzTheme {}
+  export interface DefaultTheme extends PantherTheme {}
 }
